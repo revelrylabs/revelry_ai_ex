fe83815a5ec2e73ef3a4ba73fa820498a948f65c
Christopher Stansbury <chris.stansbury@revelry.co>
2024-04-23 15:33:20 -0500

    - add get artifacts endpoint - add artifacts.ex to support - add query params and api_get (dupe #11) - update validate documentation to be same as other modules

--- lib/prodops_ex.ex
+++ lib/prodops_ex.ex
@@ -4,6 +4,7 @@ defmodule ProdopsEx do
   """
 
   alias ProdopsEx.Validate
+  alias ProdopsEx.Artifacts
 
   @doc """
   Validates the provided API key and return team information.
@@ -26,5 +27,27 @@ defmodule ProdopsEx do
   """
   def validate_api_key(config) do
     Validate.validate_api_key(config)
+  end
+
+  @doc """
+  Retrieves artifacts for a given project.
+
+  ## Parameters
+
+  - `params`: The parameters for the artifact request.
+  - `config`: The configuration map containing the API key and endpoint URL.
+
+  ## Example
+  ```
+  ProdopsEx.get_artifacts_for_project(
+    %{project_id: 212, artifact_slug: "story"},
+    %ProdopsEx.Config{
+      bearer_token: "your_api_key_here",
+    }
+  )
+  ```
+  """
+  def get_artifacts_for_project(params, config) do
+    Artifacts.get_artifacts_for_project(params, config)
   end
 end
