af55e0bd36c479540b1f815173bb276ba698f848
Jackson Oberkirch <jackson.oberkirch@revelry.co>
2024-04-23 14:37:07 -0500

    adding support for artifacts list endpoint and updating client with get

--- lib/prodops_ex.ex
+++ lib/prodops_ex.ex
@@ -3,6 +3,7 @@ defmodule ProdopsEx do
   Documentation for `ProdopsEx`.
   """
 
+  alias ProdopsEx.ArtifactType
   alias ProdopsEx.Validate
   alias ProdopsEx.Artifacts
 
@@ -49,5 +50,34 @@ defmodule ProdopsEx do
   """
   def get_artifacts_for_project(params, config) do
     Artifacts.get_artifacts_for_project(params, config)
+  end
+
+  @doc """
+  Returns a list of all artifact types for a given team
+
+  ## Parameters
+
+  - `config`: The configuration map containing the API key and endpoint URL.
+
+  ## Example
+  ```elixir
+  ProdopsEx.list_artifact_types(
+    %ProdopsEx.Config{
+      bearer_token: "your_api_key_here",
+    }
+  )
+  ```
+
+  ## Returns
+  {:ok, %{status: "ok", response: %{ "artifact_types": [
+            {
+                "slug": "story",
+                "name": "Story",
+                "description": "This is a story"
+            }
+  ]}}}
+  """
+  def list_artifact_types(config) do
+    ArtifactType.list(config)
   end
 end
